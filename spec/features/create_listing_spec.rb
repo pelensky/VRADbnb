b366993230d6ba9f3c37a6bba6afc34d8b437cab
Russell Yeo <rusyeo@gmail.com>
2016-12-12 18:28:36 +0000

    start and end dates must be provided

--- spec/features/create_listing_spec.rb
+++ spec/features/create_listing_spec.rb
@@ -21,4 +21,14 @@ feature "Creating a new listing" do
     expect(page).to have_content("Price must not be blank")
   end
 
+  scenario "Owner attempts to submit a listing without a start date" do
+    new_listing(start_date:"")
+    expect(page).to have_content("Start date must not be blank")
+  end
+
+  scenario "Owner attempts to submit a listing without an end date" do
+    new_listing(end_date:"")
+    expect(page).to have_content("End date must not be blank")
+  end
+
 end
