0eed5aaa99ad093e9b2f7b609a7e75015c21cca7
Agata Sumowska <agata.sumowska@gmail.com>
2016-12-12 14:45:47 +0000

    Form for adding a listing

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+source "https://rubygems.org"
+
+ruby '2.3.1'
+gem "sinatra"
+gem "rspec-sinatra"
+gem "rspec"
+gem "capybara"
+gem "selenium-webdriver"
--- app/vradbnb.rb
+++ app/vradbnb.rb
@@ -5,6 +5,9 @@ class VRADBnB < Sinatra::Base
     'Hello VRADBnB!'
   end
 
+  get '/listings/new' do
+    erb :create_listing
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
