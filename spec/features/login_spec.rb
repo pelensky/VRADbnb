f25a1a9d18fb8f36a7be5d99115f63ea95f4d57e
Dan Pelensky <dan@pelensky.com>
2016-12-13 13:27:45 +0000

    sign in functionality

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,11 +2,12 @@ feature "login" do
 
   scenario "user can sign in" do
     sign_up
-    visit '/sessions/new'
-    expect(page.status_code).to eq(200)
-    fill_in :email, with: 'test@test.com'
-    fill_in :password, with: '12345'
-    click_button "Sign In"
+    # sign out
+    # visit '/sessions/new'
+    # expect(page.status_code).to eq(200)
+    # fill_in :email, with: 'test@test.com'
+    # fill_in :password, with: '12345'
+    # click_button "Sign In"
     expect(page).to have_content "Aloha, test@test.com"
   end
 
@@ -16,6 +17,13 @@ feature "login" do
     fill_in :password, with: 'wrong'
     click_button "Sign In"
     expect(page).to have_content "The email or password is incorrect"
+  end
+
+  scenario "user can not sign in if they are already signed it" do
+    sign_up
+
+
+
   end
 
 
