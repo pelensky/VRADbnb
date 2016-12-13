57c8628e6b6b83d2f2ca9fbb8b4e7e96fdd5931f
Agata Sumowska <agata.sumowska@gmail.com>
2016-12-13 16:08:20 +0000

    wip

--- spec/features/filtering_spec.rb
+++ spec/features/filtering_spec.rb
@@ -6,4 +6,15 @@ feature "Filter spaces" do
     expect(page).to have_content("victorian")
     expect(page).to have_content("palace")
   end
+
+  scenario "renter can see all spaces" do
+    sign_up
+    new_listing
+    new_listing2
+    fill_in("start_date",with: "05/12/2016")
+    fill_in("end_date",with: "06/12/2016")
+    click_button("List Spaces")
+    expect(page).to have_content("victorian")
+    expect(page).not_to have_content("palace")
+  end
 end
