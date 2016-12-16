08d921c2e475741bb5d9ee3c38aa2a93e8597aa2
Agata Sumowska <agata.sumowska@gmail.com>
2016-12-16 11:11:44 +0000

    renter routing fixed

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -2,8 +2,9 @@ class VRADBnB < Sinatra::Base
 
     post '/booking/:id' do
       listing = Listing.first(params[:id])
-      book = Book.create(date: params[:date], renter: current_renter, listing: listing)
-      redirect to('/booking/:renter_id')
+      book = Book.create(date: params[:date],
+            renter: current_renter, listing: listing)
+      redirect to('/booking/history')
     end
 
     post '/booking' do
@@ -11,8 +12,8 @@ class VRADBnB < Sinatra::Base
       erb :booking
     end
 
-    get '/booking/:renter_id' do
-      @renter_history = Book.all(renter_id: session[:renter_id])
+    get '/booking/history' do
+      @renter_history = Book.all(renter_id: current_renter.id)
       erb :history
     end
 end
--- spec/features/renter_history_spec.rb
+++ spec/features/renter_history_spec.rb
@@ -2,7 +2,7 @@ feature "renter's history page" do
 
   scenario "should see a list of all the renter's bookings" do
     book_a_place
-    expect(current_path).to eq('/booking/4')
+    expect(current_path).to eq('/booking/history')
     expect(page).to have_content("Sunny palace")
   end
 end
