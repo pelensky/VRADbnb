e199d8862aa5c10124ce7216c9eadcc9e27c95f1
Russell Yeo <rusyeo@gmail.com>
2016-12-16 16:28:46 +0000

    owner can confirm a booking, close #52, close #47

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -2,6 +2,7 @@ class VRADBnB < Sinatra::Base
      post '/booking/new' do
       listing = Listing.get(params[:hidden_value])
       listing.requested = true
+      listing.save
       book = Book.create(date: params[:date],
             renter: current_renter, listing: listing)
       redirect to('/booking/renter_history')
@@ -14,16 +15,21 @@ class VRADBnB < Sinatra::Base
 
     get '/booking/renter_history' do
       @renter_history = Book.all(renter_id: current_renter.id)
-
       erb :renter_history
     end
 
     get '/booking/owner_history' do
       owner_history = Listing.all(owner_id: current_owner.id)
-
       requested = owner_history.all(requested: true)
       @owner_bookings = []
       requested.each {|listing| @owner_bookings<< Book.first(listing_id: listing.id)}
       erb :owner_history
+    end
+
+    post '/booking/confirm' do
+      booking = Book.first(id: params[:booking_id])
+      booking.confirmed = true
+      booking.save
+      redirect '/booking/owner_history'
     end
 end
--- spec/features/requests/booking_confirmation_spec.rb
+++ spec/features/requests/booking_confirmation_spec.rb
@@ -3,9 +3,7 @@ feature "When a renter has made a booking" do
   scenario "owner can see a request" do
     book_a_place
     sign_in_o
-
     visit '/booking/owner_history'
-
     expect(page).to have_content("Sunny palace")
     expect(page).to have_button("Confirm")
   end
@@ -16,5 +14,14 @@ feature "When a renter has made a booking" do
     expect(listing.requested).to eq(true)
   end
 
-
+  scenario "owner confirms" do
+    book_a_place
+    sign_in_o
+    visit '/booking/owner_history'
+    click_button('Confirm')
+    sign_in_r
+    visit('/booking/renter_history')
+    expect(page).to have_content("Sunny palace")
+    expect(page).to have_content("Confirmed")
+  end
 end
