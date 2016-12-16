26c876770c9d4e687599ba9979cee59a32a016da
Dan Pelensky <dan@pelensky.com>
2016-12-16 12:05:48 +0000

    correct listing id

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -1,9 +1,10 @@
 class VRADBnB < Sinatra::Base
 
-    post '/booking/:id' do
-      listing = Listing.first(params[:id])
+    post '/booking/new' do
+      listing = Listing.get(params[:hidden_value])
       book = Book.create(date: params[:date],
             renter: current_renter, listing: listing)
+            # binding.pry
       redirect to('/booking/history')
     end
 
@@ -14,6 +15,7 @@ class VRADBnB < Sinatra::Base
 
     get '/booking/history' do
       @renter_history = Book.all(renter_id: current_renter.id)
+      # binding.pry
       erb :history
     end
 end
