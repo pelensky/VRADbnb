3a182c2f9abe8a97f52f79405981598a9960c64b
Agata Sumowska <agata.sumowska@gmail.com>
2016-12-16 13:31:21 +0000

    adding requested and confirmed property to listing

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -2,6 +2,7 @@ class VRADBnB < Sinatra::Base
 
     post '/booking/:id' do
       listing = Listing.first(params[:id])
+      listing.requested = true
       book = Book.create(date: params[:date],
             renter: current_renter, listing: listing)
       redirect to('/booking/history')
@@ -13,7 +14,17 @@ class VRADBnB < Sinatra::Base
     end
 
     get '/booking/history' do
-      @renter_history = Book.all(renter_id: current_renter.id)
-      erb :history
+        @renter_history = Book.all(renter_id: current_renter.id)
+        erb :renter_history
+
+
+
+    end
+
+    get '/booking/history/owner' do
+      binding.pry
+      owner_history = Book.all(listing_id: current_owner.listings)
+      @owner_requested_spaces = owner_history.all(requested: true)
+      erb :owner_history
     end
 end
